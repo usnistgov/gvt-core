e7a1109cc210da6e4e43e6429d45b4992a01942b
Nicolas Crouzier <crouzier@pn125940.campus.nist.gov>
2024-10-28 12:27:56 -0400

    no message

--- src/main/java/gov/nist/hit/gvt/core/GVTResourceLoaderImpl.java
+++ src/main/java/gov/nist/hit/gvt/core/GVTResourceLoaderImpl.java
@@ -105,12 +105,20 @@ public class GVTResourceLoaderImpl extends GVTResourceLoader {
 		return hl7v2rb.vocabLibrary(content, domain, scope, authorUsername, preloaded);
 	}
 
+//    @Override
+//    public ProfileModel parseEnhanced(String integrationProfileXml, String conformanceProfileId,
+//            String constraintsXml, String additionalConstraintsXml, String valueSets, String valueSetBindings, String coConstraints,
+//            String slicings) throws ProfileParserException, UnsupportedOperationException {
+//        return hl7v2rb.parseEnhanced(integrationProfileXml, conformanceProfileId, constraintsXml,
+//                additionalConstraintsXml,valueSets,valueSetBindings, coConstraints, slicings);
+//    }
+
     @Override
-    public ProfileModel parseEnhanced(String integrationProfileXml, String conformanceProfileId,
-            String constraintsXml, String additionalConstraintsXml, String valueSetBindings, String coConstraints,
-            String slicings) throws ProfileParserException, UnsupportedOperationException {
+    public ProfileModel parseEnhanced(String integrationProfileXml, String conformanceProfileId, String constraintsXml,
+            String additionalConstraintsXml, String valueSetBindings, String coConstraints, String slicings)
+            throws ProfileParserException, UnsupportedOperationException {
         return hl7v2rb.parseEnhanced(integrationProfileXml, conformanceProfileId, constraintsXml,
-                additionalConstraintsXml,valueSetBindings, coConstraints, slicings);
+              additionalConstraintsXml,valueSetBindings, coConstraints, slicings);
     }
 
    
